1bc55108b1dee0ae349b7e57403a54aaafe5c64e
Meph1sto <robert.heeley@gmail.com>
2018-03-20 23:56:14 +0000

    v 1.0

--- pf/pf/settings.py
+++ pf/pf/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ["mephist0.pythonanywhere.com",
 INSTALLED_APPS = [
     'search.apps.SearchConfig',
     'django_filters',
-    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
