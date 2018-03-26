dd719aea145833c3ed01d6889ba3c19fdb6d287d
Meph1sto <robert.heeley@gmail.com>
2018-03-26 18:18:45 +0100

    Update settings.py

--- pf/pf/settings.py
+++ pf/pf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a75*lx9en%ci6pgnz4!294&hy#kqmjk^*g#7m6m7t*@fr!%5n$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["mephist0.pythonanywhere.com",
                  "127.0.0.1". 
